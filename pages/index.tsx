5e5904f109ca1d65d7dbe18c080d91c9b012f825
ikbakkk <iqbalfirdaus05@gmail.com>
2023-01-04 16:58:58 +0800

    Add typing def

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,47 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Banner from '../components/Banner'
-import Posts from '../components/Posts'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Banner from '../components/Banner';
+import Posts from '../components/Posts';
+import { sanityClient, urlFor } from '../sanity';
+import { Post } from '../typing';
 
-const Home: NextPage = () => {
-  return (
-    <div className='mx-auto max-w-7xl'>
-      <Head>
-        <title>Medium Blog</title>
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-      <Banner />
-      <Posts />
-    </div>
-  )
+interface Props {
+	posts: Post[];
 }
 
-export default Home
+const Home = ({ posts }: Props) => {
+	return (
+		<div className='mx-auto max-w-7xl'>
+			<Head>
+				<title>Medium Blog</title>
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+			<Banner />
+			<Posts />
+		</div>
+	);
+};
+
+export default Home;
+
+export const getServerSideProps = async () => {
+	const query = `*[_type =='post' ]{
+    _id,
+    title,
+    author,
+    author=>{
+      name,
+      image
+    },
+    description,
+    mainImage,
+    slug,
+  }`;
+
+	const posts = await sanityClient.fetch(query);
+	return {
+		props: {
+			posts
+		}
+	};
+};
